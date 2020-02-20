9994b084d31f499a832d7da4ace260a97c2e0b09
Roberto Polli <robipolli@gmail.com>
2020-02-20 14:56:49 +0100

    parse yaml

--- index.js
+++ index.js
@@ -1,33 +1,31 @@
-const uniq = require('uniq');
-
-function myfunc() {
-  return uniq([1, 2, 2, 3]).join(' ');
-}
-exports.myfunc = myfunc;
-
-const { Spectral } = require('@stoplight/spectral');
+const { Spectral,  isOpenApiv2, isOpenApiv3 } = require('@stoplight/spectral');
 const { getLocationForJsonPath, parseWithPointers } = require("@stoplight/yaml");
 
 
+
 function parse(oas3){
-return new Promise((resolve, reject) => {
-	const myOpenApiDocument = parseWithPointers(oas3);
-	const spectral = new Spectral();
-	spectral
-	  .run({
-	    parsed: myOpenApiDocument,
-	    getLocationForJsonPath,
-	  })
-	  .then((out) => {
-		console.log(out);
-		resolve(out);
+	return new Promise((resolve, reject) => {
+		const myOpenApiDocument = parseWithPointers(oas3);
+		const spectral = new Spectral();
+		spectral.registerFormat('oas2', isOpenApiv2);
+		spectral.registerFormat('oas3', isOpenApiv3);
+
+		spectral.loadRuleset("https://gist.githubusercontent.com/ioggstream/88087a229643dc3186290acdb5d0de1f/raw/7b5d24185e2b8debf86ba6a639ce0b9837d7a452/ruleset.yml")
+		  .then(() => spectral
+			  .run({
+				parsed: myOpenApiDocument,
+				getLocationForJsonPath,
+			  })
+			  .then((out) => {
+				console.log(out);
+				resolve(out);
+			}));
 	});
-})	
 }
 exports.parse = parse;
 
 
-async function parse_url(url, ){
+function parse_url(url, ){
 	fetch(url).then(function(r){
                 r.text().then(function(oas){
 			console.log("url: " + url); 
