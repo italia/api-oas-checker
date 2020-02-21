de8c7f7b8160069669b972582d9417f0a12c87c0
Roberto Polli <robipolli@gmail.com>
2020-02-21 08:02:33 +0100

    mv ruleset

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const { getLocationForJsonPath, parseWithPointers } = require("@stoplight/yaml")
 
 
 
-function parse(oas3){
+function parse(oas3, ruleset){
+	ruleset = ruleset || "https://raw.githubusercontent.com/ioggstream/oas-spectral-validator/master/.spectral.yml"
+
 	return new Promise((resolve, reject) => {
 		const myOpenApiDocument = parseWithPointers(oas3);
 		const spectral = new Spectral();
 		spectral.registerFormat('oas2', isOpenApiv2);
 		spectral.registerFormat('oas3', isOpenApiv3);
-        const ruleset = "https://raw.githubusercontent.com/ioggstream/oas-spectral-validator/master/ruleset.yml"
 		spectral.loadRuleset(ruleset)
 		  .then(() => spectral
 			  .run({
@@ -25,12 +26,12 @@ function parse(oas3){
 exports.parse = parse;
 
 
-function parse_url(url, ){
+function parse_url(url){
 	fetch(url).then(function(r){
                 r.text().then(function(oas){
 			console.log("url: " + url); 
 			console.log("oas: " + oas); 
-			lint = browserify_hello_world.parse(oas); 
+			lint = browserify_hello_world.parse(oas, null); 
 			console.log(lint); 
 		});
 	});
