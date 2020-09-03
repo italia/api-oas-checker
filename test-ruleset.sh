c415553102b2952ffb76a622313cf088c4da5eb1
Roberto Polli <robipolli@gmail.com>
2020-09-03 17:32:42 +0200

    Support multiple testdirs

--- test-ruleset.sh
+++ test-ruleset.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 shopt -s extglob
-export PATH="$PATH:./node_modules/.bin:"
-RULES=$(cd rules; echo *.yml | sed -e 's/.yml\b//g')
+export PATH="$PATH:$PWD/node_modules/.bin:"
+
+BASEDIR="${1?Missing base directory}"; shift
+
+cd "$BASEDIR" || {
+  echo "missing directory $BASEDIR"
+  exit 1
+}
+
+RULES=$(echo *.yml | sed -e 's/.yml\b//g')
 RULES_REGEXP="${RULES// /|}"
 echo >&2 "$RULES"
 
@@ -10,29 +18,29 @@ case "$1" in
     "--snapshot")
         if [ "$2" != "" ]; then
             RULE="$2"
-            spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml > rules/tests/$RULE-test.snapshot
+            spectral lint tests/$RULE-test.yml -r $RULE.yml > tests/$RULE-test.snapshot
             exit 0
         fi
         for RULE in $RULES; do
-            spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml > rules/tests/$RULE-test.snapshot
+            spectral lint tests/$RULE-test.yml -r $RULE.yml > tests/$RULE-test.snapshot
         done
         exit 0
         ;;
     all)
         for RULE in $RULES; do
-            spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml | \
-                diff --color -I '^.*rules/tests/.*-test.yml$' "rules/tests/$RULE-test.snapshot" -
+            spectral lint tests/$RULE-test.yml -r $RULE.yml | \
+                diff --color -I '^.*tests/.*-test.yml$' "tests/$RULE-test.snapshot" -
         done
         ;;
     @($RULES_REGEXP))
         RULE="$1"
 
-        if [ ! -f "rules/tests/$RULE-test.snapshot" ]; then
+        if [ ! -f "tests/$RULE-test.snapshot" ]; then
             echo "Missing test snapshot for rule: $RULE"
             exit 1
         fi
-        spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml | \
-            diff -wubBEr --color -I '^.*rules/tests/.*-test.yml$' "rules/tests/$RULE-test.snapshot" -
+        spectral lint tests/$RULE-test.yml -r $RULE.yml | \
+            diff -wubBEr --color -I '^.*tests/.*-test.yml$' "tests/$RULE-test.snapshot" -
         TEST_OUT="$?"
         if [ "$TEST_OUT" != "0" ]; then
             echo "Unexpected test result"
