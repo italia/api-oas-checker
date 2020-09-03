1add9e68dbc41cf525fab81627929a76f6df81c1
Roberto Polli <robipolli@gmail.com>
2020-09-03 17:18:26 +0200

    Test refactor (#56)
    * Move test in a separate dir, refactor test

--- test-ruleset.sh
+++ test-ruleset.sh
@@ -1,39 +1,38 @@
 #!/bin/bash
 shopt -s extglob
 export PATH="$PATH:./node_modules/.bin:"
-RULES=$(ls rules/*-test.yml | awk -F'[/-]' '{print $2}')
-RULES_L="$(echo $RULES)"
-RULES_REGEXP="${RULES_L// /|}"
-echo >&2 "$RULES_L"
+RULES=$(cd rules; echo *.yml | sed -e 's/.yml\b//g')
+RULES_REGEXP="${RULES// /|}"
+echo >&2 "$RULES"
 
 case "$1" in
 
     "--snapshot")
         if [ "$2" != "" ]; then
             RULE="$2"
-            spectral lint rules/$RULE-test.yml -r rules/$RULE.yml > rules/$RULE-test.snapshot
+            spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml > rules/tests/$RULE-test.snapshot
             exit 0
         fi
         for RULE in $RULES; do
-            spectral lint rules/$RULE-test.yml -r rules/$RULE.yml > rules/$RULE-test.snapshot
+            spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml > rules/tests/$RULE-test.snapshot
         done
         exit 0
         ;;
     all)
         for RULE in $RULES; do
-            spectral lint rules/$RULE-test.yml -r rules/$RULE.yml | \
-                diff --color -I '^.*rules/.*-test.yml$' "rules/$RULE-test.snapshot" -
+            spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml | \
+                diff --color -I '^.*rules/tests/.*-test.yml$' "rules/tests/$RULE-test.snapshot" -
         done
         ;;
     @($RULES_REGEXP))
         RULE="$1"
 
-        if [ ! -f "rules/$RULE-test.snapshot" ]; then
+        if [ ! -f "rules/tests/$RULE-test.snapshot" ]; then
             echo "Missing test snapshot for rule: $RULE"
             exit 1
         fi
-        spectral lint rules/$RULE-test.yml -r rules/$RULE.yml | \
-            diff -wubBEr --color -I '^.*rules/.*-test.yml$' "rules/$RULE-test.snapshot" -
+        spectral lint rules/tests/$RULE-test.yml -r rules/$RULE.yml | \
+            diff -wubBEr --color -I '^.*rules/tests/.*-test.yml$' "rules/tests/$RULE-test.snapshot" -
         TEST_OUT="$?"
         if [ "$TEST_OUT" != "0" ]; then
             echo "Unexpected test result"
@@ -41,7 +40,7 @@ case "$1" in
         fi
     ;;
     *)
-        echo >&2 "Please specify a rule in $RULES_L"
+        echo >&2 "Please specify a rule in: $RULES"
         exit 1
     ;;
 esac
