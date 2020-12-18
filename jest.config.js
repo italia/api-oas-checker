380ddddf55c937b1369593a31c3f2bb1f280698e
emilioSp <emilio.spatola@gmail.com>
2020-12-18 15:08:47 +0100

    First test

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  transformIgnorePatterns: [
+    "/node_modules/(?!monaco-editor)/",
+  ],
+  //ignore css imported in components
+  moduleNameMapper: {
+    "\\.(css|less|scss|sass)$": "identity-obj-proxy",
+    "monaco-editor": "monaco-editor/esm/vs/editor/editor.api.js"
+  }
+}
