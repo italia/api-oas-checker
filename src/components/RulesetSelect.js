31a553584012de0f138ad2cacab822bd0ba9d7c6
emilioSp <emilio.spatola@gmail.com>
2021-01-04 18:43:21 +0100

    wip

--- src/components/RulesetSelect.js
+++ src/components/RulesetSelect.js
@@ -1,21 +1,11 @@
-import React, { useCallback } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { isValidationInProgress, getRuleset } from '../redux/selectors.js';
 import { setRuleset } from '../redux/actions.js';
-import {
-  getDocFilename,
-  RULESET_BEST_PRACTICES,
-  RULESET_ITALIAN,
-  RULESET_ITALIAN_PLUS_SECURITY,
-  RULESET_SECURITY,
-} from '../utils.mjs';
-import { Button } from 'design-react-kit';
+import { RULESET_BEST_PRACTICES, RULESET_ITALIAN, RULESET_ITALIAN_PLUS_SECURITY, RULESET_SECURITY } from '../utils.mjs';
 
 const RulesetSelect = ({ isValidationInProgress, ruleset, setRuleset }) => {
-  const openDoc = useCallback(() => {
-    window.open(getDocFilename(ruleset));
-  }, [ruleset]);
   return (
     <>
       <div className="bootstrap-select-wrapper mt-4 px-3">
@@ -32,9 +22,6 @@ const RulesetSelect = ({ isValidationInProgress, ruleset, setRuleset }) => {
           <option value={RULESET_ITALIAN_PLUS_SECURITY}>Italian API Guidelines + Extra Security Checks</option>
         </select>
       </div>
-      <Button onClick={openDoc} color="primary" icon={false} tag="button">
-        Open Doc
-      </Button>
     </>
   );
 };
