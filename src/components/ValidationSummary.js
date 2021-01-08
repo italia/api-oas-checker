7eee346a930ab31c48913bb4344177b61f009338
emilioSp <emilio.spatola@gmail.com>
2021-01-08 11:00:48 +0100

    fix: css styles for errors and warnings on no problems detected

--- src/components/ValidationSummary.js
+++ src/components/ValidationSummary.js
@@ -5,39 +5,43 @@ import { getValidationResults } from '../redux/selectors.js';
 import { createUseStyles } from 'react-jss';
 import { ERROR, WARNING } from '../utils.mjs';
 import { getValidationResultsPropTypes, getValidationResultType } from '../spectral/spectral_utils.js';
+
 const useStyles = createUseStyles({
   error: {
-    backgroundColor: 'var(--danger)',
+    backgroundColor: (summary) => (summary.errors > 0 ? 'var(--danger)' : 'var(--success)'),
   },
   warning: {
-    backgroundColor: 'var(--warning)',
-    color: 'var(--warning-text-dark)',
+    backgroundColor: (summary) => (summary.warnings > 0 ? 'var(--warning)' : 'var(--success)'),
+    color: (summary) => (summary.warnings > 0 ? 'var(--warning-text-dark)' : 'var(--white)'),
   },
 });
 
+const getErrorLabel = (summary) => (summary.errors > 0 ? `${summary.errors} errors` : 'No errors');
+
+const getWarningLabel = (summary) => (summary.warnings > 0 ? `${summary.warnings} warnings` : 'No warnings');
+
 const ValidationSummary = ({ validationResults }) => {
-  const classes = useStyles();
-
-  if (validationResults === null) return null;
-
   const summary = {
-    errors: validationResults.filter((r) => getValidationResultType(r.severity) === ERROR).length,
-    warnings: validationResults.filter((r) => getValidationResultType(r.severity) === WARNING).length,
+    errors: validationResults?.filter((r) => getValidationResultType(r.severity) === ERROR).length,
+    warnings: validationResults?.filter((r) => getValidationResultType(r.severity) === WARNING).length,
   };
+  const classes = useStyles(summary);
 
   return (
-    <div className="d-flex p-3 bg-primary">
-      <h4 className="pr-3">
-        <Badge data-testid="errors-badge" className={classes.error} pill={false} tag="span">
-          {summary.errors} errors
-        </Badge>
-      </h4>
-      <h4>
-        <Badge data-testid="warnings-badge" className={classes.warning} pill={false} tag="span">
-          {summary.warnings} warnings
-        </Badge>
-      </h4>
-    </div>
+    validationResults && (
+      <div className="d-flex p-3 bg-primary">
+        <h4 className="pr-3">
+          <Badge data-testid="errors-badge" className={classes.error} pill={false} tag="span">
+            {getErrorLabel(summary)}
+          </Badge>
+        </h4>
+        <h4>
+          <Badge data-testid="warnings-badge" className={classes.warning} pill={false} tag="span">
+            {getWarningLabel(summary)}
+          </Badge>
+        </h4>
+      </div>
+    )
   );
 };
 
