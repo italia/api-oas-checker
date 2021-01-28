50ce9b131ba334ed0985d3e0f8491e3500ed9a9e
Roberto Polli <robipolli@gmail.com>
2021-01-28 22:29:35 +0100

    improve test

--- test-ruleset.sh
+++ test-ruleset.sh
@@ -4,6 +4,15 @@ export PATH="$PATH:$PWD/node_modules/.bin:"
 
 BASEDIR="${1?Missing base directory}"; shift
 
+do_or_die(){
+	local TEST_OUT=$1
+        if [ "$TEST_OUT" != "0" ]; then
+            echo "X-(        TEST ERROR      X-("
+            echo "X-( Unexpected test result X-("
+            exit 1
+        fi
+}
+
 cd "$BASEDIR" || {
   echo "missing directory $BASEDIR"
   exit 1
@@ -11,25 +20,28 @@ cd "$BASEDIR" || {
 
 RULES=$(echo *.yml | sed -e 's/.yml\b//g')
 RULES_REGEXP="${RULES// /|}"
-echo >&2 "available rules: $RULES"
 
 case "$1" in
 
     "--snapshot")
         if [ "$2" != "" ]; then
             RULE="$2"
+            echo -n "Snapshotting rule $RULE.."
             spectral lint tests/$RULE-test.yml -r $RULE.yml > tests/$RULE-test.snapshot
             exit 0
         fi
         for RULE in $RULES; do
+            echo -n "Snapshotting rule $RULE.."
             spectral lint tests/$RULE-test.yml -r $RULE.yml > tests/$RULE-test.snapshot
         done
         exit 0
         ;;
     all)
         for RULE in $RULES; do
+            echo -n "Executing rule $RULE.."
             spectral lint tests/$RULE-test.yml -r $RULE.yml | \
-                diff --color -I '^.*tests/.*-test.yml$' "tests/$RULE-test.snapshot" -
+                diff --color -I '^.*tests/.*-test.yml$' "tests/$RULE-test.snapshot" - && echo "OK"
+            do_or_die "$?"
         done
         ;;
     @($RULES_REGEXP))
@@ -42,11 +54,7 @@ case "$1" in
         spectral lint tests/$RULE-test.yml -r $RULE.yml | \
             diff -wubBEr --color -I '^.*tests/.*-test.yml$' "tests/$RULE-test.snapshot" -
         TEST_OUT="$?"
-        if [ "$TEST_OUT" != "0" ]; then
-            echo "X-(        TEST ERROR      X-("
-            echo "X-( Unexpected test result X-("
-            exit 1
-        fi
+    	do_or_die $TEST_OUT
         echo "Ok"
         exit 0
     ;;
