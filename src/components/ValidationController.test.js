e80695ee4451b5b7b2ef3b01bf0319dfb5da902d
Andrea Stagi <stagi.andrea@gmail.com>
2022-03-14 14:23:34 +0100

    chore: use act to render components in testing

--- src/components/ValidationController.test.js
+++ src/components/ValidationController.test.js
@@ -1,25 +1,28 @@
 import React from 'react';
+import { act } from 'react-dom/test-utils';
 
 import { render, screen, fireEvent } from '../test-utils.js';
 import { ValidationController } from './ValidationController.js';
 
 describe('ValidationController', () => {
   // TODO mock redux dispatch
-  xit('calls on validate function', () => {
+  xit('calls on validate function', async () => {
     const onValidateMock = jest.fn();
-    render(<ValidationController onValidate={onValidateMock} />);
+    await act(async () => render(<ValidationController onValidate={onValidateMock} />));
     fireEvent.click(screen.queryByTestId('validation-button'));
     expect(onValidateMock).toHaveBeenCalledTimes(1);
   });
 
-  it('displays please wait', () => {
-    render(<ValidationController />, {
-      initialState: {
-        validationState: {
-          inProgress: true,
+  it('displays please wait', async () => {
+    await act(async () =>
+      render(<ValidationController />, {
+        initialState: {
+          validationState: {
+            inProgress: true,
+          },
         },
-      },
-    });
+      })
+    );
     expect(screen.getByText(/please wait/i)).toBeTruthy();
     expect(screen.queryByTestId('auto-refresh')).toBeNull();
   });
