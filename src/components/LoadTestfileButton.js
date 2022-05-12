d13a3708951edfc157443e50207366262409cfd9
Roberto Polli <robipolli@gmail.com>
2022-05-12 20:50:06 +0200

    Add mock component

--- src/components/LoadTestfileButton.js
+++ src/components/LoadTestfileButton.js
@@ -1,13 +1,31 @@
 import React, { useCallback, useState } from 'react';
-import { Dropdown, DropdownItem, DropdownMenu, DropdownToggle, Icon } from 'design-react-kit';
+import {
+  Button,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Icon,
+  Modal,
+  ModalBody,
+  ModalFooter,
+  ModalHeader,
+} from 'design-react-kit';
+import SwaggerUI from 'swagger-ui-react';
 import { useSelector, useDispatch } from 'react-redux';
 import { setDocumentUrl, resetValidationResults, setTemplateDocumentName } from '../redux/actions.js';
 import { isValidationInProgress } from '../redux/selectors.js';
 import { TEMPLATE_DOCUMENT_URL } from '../utils.mjs';
+import useModalView from './useModalView.js';
 
 export const LoadTestfileButton = () => {
   const validationInProgress = useSelector((state) => isValidationInProgress(state));
   const dispatch = useDispatch();
+  const [isModalOpen, closeModal, openModal] = useModalView();
+  const handleConfirmAction = useCallback(() => {
+    closeModal();
+  }, [closeModal]);
+
   const handleOnClick = useCallback(
     (url) => {
       console.log(`Loading ${url}`);
@@ -17,7 +35,18 @@ export const LoadTestfileButton = () => {
     },
     [dispatch]
   );
+  const schema = {
+    type: 'object',
+    properties: {
+      url: {
+        type: 'string',
+      },
+    },
+  };
 
+  /*
+
+              */
   const [open, toggle] = useState(false);
   return (
     <Dropdown isOpen={open} toggle={() => toggle(!open)}>
@@ -30,6 +59,24 @@ export const LoadTestfileButton = () => {
         </DropdownItem>
         <DropdownItem onClick={() => handleOnClick('errorfile.yaml')}>
           <Icon className="left" icon="it-upload" aria-hidden size="sm" /> Example API with error
+        </DropdownItem>
+
+        <DropdownItem onClick={openModal}>
+          <Icon className="left" icon="it-upload" aria-hidden size="sm" />
+          Show schema
+          <Modal fade={false} isOpen={isModalOpen} role="dialog" centered toggle={closeModal}>
+            <ModalHeader charCode={215} closeAriaLabel="Close" tag="h5" wrapTag="div" toggle={closeModal}>
+              Mostra schema
+            </ModalHeader>
+            <ModalBody className="mt-3" tag="div">
+              <SwaggerUI />
+            </ModalBody>
+            <ModalFooter tag="div">
+              <Button color="primary" icon={false} onClick={handleConfirmAction} tag="button">
+                Close
+              </Button>
+            </ModalFooter>
+          </Modal>
         </DropdownItem>
       </DropdownMenu>
     </Dropdown>
