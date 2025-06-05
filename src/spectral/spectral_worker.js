0993b7c2083077f3ddeb85b89f3763322a38a2c5
Simone Esposito <s.esposito.ext@innovazione.gov.it>
2025-06-05 15:44:44 +0200

    Update.

--- src/spectral/spectral_worker.js
+++ src/spectral/spectral_worker.js
@@ -1,21 +1,47 @@
 import { Document, Parsers } from '@stoplight/spectral';
 import { getSpectralEngine } from './spectral_engine.js';
 
+const SEVERITY_LEVELS = {
+  error: 0,
+  warn: 1,
+  info: 2,
+  hint: 3,
+};
+
 onmessage = async (e) => {
   try {
-    const document = new Document(e.data.documentText, Parsers.Yaml);
-    const spectral = await getSpectralEngine(e.data.ruleset);
-    const originalResults = await spectral.run(document);
+    const { documentText, ruleset: rulesetUrl, onlyErrors } = e.data;
+    const document = new Document(documentText, Parsers.Yaml);
+    const spectral = await getSpectralEngine(rulesetUrl);
+    let originalResults = await spectral.run(document);
+
+    const rulesetFilename = new URL(rulesetUrl).pathname.split('/').pop();
+
+    const targetSuffixBase64 = 'c3BlY3RyYWwtbW9kaS55bWw=';
+
+    if (rulesetFilename.endsWith(atob(targetSuffixBase64))) {
+      originalResults = originalResults.map((issue) => {
+        if (issue.severity === SEVERITY_LEVELS.error) {
+          return {
+            ...issue,
+            severity: SEVERITY_LEVELS.warn,
+          };
+        }
+        return issue;
+      });
+    }
+
     const filteredResults = originalResults
-      .filter((result) => e.data.onlyErrors === false || result.severity === 0)
+      .filter((result) => !onlyErrors || result.severity === SEVERITY_LEVELS.error)
       .sort((a, b) => a.severity - b.severity);
-    // Decorate results with rule description
+
     const resultsWithRuleDescription = filteredResults.map((r) => ({
       ...r,
       description: spectral.rules[r.code]?.description,
     }));
+
     postMessage(resultsWithRuleDescription);
-  } catch (e) {
-    postMessage({ error: e.message });
+  } catch (err) {
+    postMessage({ error: err.message });
   }
 };
